929f748582315c62abad66e3a386a169882d2acb
luolisave <luolisave@gmail.com>
2024-03-29 13:43:46 -0400

    lombok for getter and setter annotations.

--- src/main/java/com/ltp/contacts/pojo/Atom.java
+++ src/main/java/com/ltp/contacts/pojo/Atom.java
@@ -8,10 +8,19 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 // insert into atom values('1', 'content content content 1', 'desc 1', 'title 1')
 // insert into atom values('2', 'content content content 2', 'desc 2', 'title 2')
 @Entity
 @Table(name = "atom")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Atom {
 
     @Id
@@ -28,37 +37,5 @@ public class Atom {
     // private String content;
     @Column(name = "content", columnDefinition = "TEXT") // TEXT uses CHARACTER VARYING internally. 1GB max
     private String content;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDesc() {
-        return this.title;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
 }
