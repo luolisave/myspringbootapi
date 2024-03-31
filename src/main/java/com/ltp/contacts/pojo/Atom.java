a674010be28bfd9b6dd91883cd3de6e6bf697f85
luolisave <luolisave@gmail.com>
2024-03-31 12:20:24 -0400

    upgrade to java 21

--- src/main/java/com/ltp/contacts/pojo/Atom.java
+++ src/main/java/com/ltp/contacts/pojo/Atom.java
@@ -2,14 +2,14 @@ package com.ltp.contacts.pojo;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
